30f37358b0aef6a919ad85488f4ba467fe561c45
pabsilop <pablosilva2705@hotmail.com>
2020-11-13 18:02:07 +0100

    Entrega

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ import {Ticket} from './modelos/ticket.js';
 
 //Se inicializan los datos.
 let parking = createParking();
-
+let contrasenia="1234";
 let opt = readline.question('|1 - Zona Usuario| |2 - Zona Admin|');
 console.log(opt);
 
@@ -25,7 +25,7 @@ switch (opt) {
 
     case '1':
         
-        let opt2 = readline.question('|1 - Depositar Vehiculo|')
+        let opt2 = readline.question('| 1 - Depositar Vehiculo | | 2 - Retirar Vehiculo | | 3 - Depositar Abonados | | 4 - Retirar Abonados |')
 
         switch (opt2) {
             case '1':
@@ -41,11 +41,42 @@ switch (opt) {
             case '4':
 
                 break;
+            case '0':
+
+                break;
         }
 
         break;
     case '2':
+       let peticion= readline.question('Introduzca la contraseña de adminisitrador del programa\n')
+        if(peticion==contrasenia){
+            let opt3 = readline.question('| 1 - Estado del Parking | | 2 - Facturación | | 3 - Consulta de Abonados | | 4 - Abonos | | 5 - Calidad de Abonos |')
+            switch (opt3) {
+                case '1':
+                    console.log(imprimirPlazasDisponibles(parking));
+                   
+                    break;
+                case '2':
+    
+                    break;
+                case '3':
+    
+                    break;
+                case '4':
+    
+                    break;
+                case '5':
 
+                    break;
+                case '0':
+
+                    break;
+            }
+        }else{
+            console.log("Contraseña de admin incorrecta, no puedes pasar")
+        }
+        
+       
         break;
 }
 
